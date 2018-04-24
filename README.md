fcdb3d4cf80e0c3b274994657dd9bbf9ff1bc686
Krzysztof Kowalczyk <krzysztof.kowalczyk@bzwbk.pl>
2018-04-24 13:54:53 +0200

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_second
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef_second
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
